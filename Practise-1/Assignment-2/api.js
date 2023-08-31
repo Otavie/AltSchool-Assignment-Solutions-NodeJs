87a13d8376c7cc37a437a14503ed7145c9f1392e
Otavie <otavieokuoyo@gmail.com>
2023-08-31 10:36:51 +0100

    Add code for PUT method

--- Practise-1/Assignment-2/api.js
+++ Practise-1/Assignment-2/api.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-const { error } = require('console');
 const PORT = 3131;
 const studentsFile = './students.json';
 
@@ -16,6 +15,10 @@ const server = http.createServer((req, res) =>{
 
     if (req.url.startsWith('/students/') && req.method === "GET") {
         GetOneStudent(req, res);
+    }
+
+    if (req.url.startsWith('/students/') && req.method === "PUT") {
+        PartUpdate(req, res);
     }
 })
 
@@ -36,7 +39,7 @@ function GetAllStudents(req, res) {
 
 // Add a Student
 function AddStudent(req, res) {
-    fs.readFile(studentsFile, 'utf-8', (error, fileData) => {
+    fs.readFile(studentsFile, 'utf-8', (error) => {
         if (error) {
             console.log(error);
             return;
@@ -86,7 +89,6 @@ function AddStudent(req, res) {
 function GetOneStudent(req, res) {
     const urlID = req.url.split('/')[2]
     
-
     fs.readFile(studentsFile, 'utf-8', (error, fileData) => {
         if (error) {
             console.log(error);
@@ -103,6 +105,54 @@ function GetOneStudent(req, res) {
         } else {
             res.writeHead(404).write('Student Not Found!');
         }
+        res.end();
+    })
+}
+
+// Complete or Partial Update
+function PartUpdate(req, res) {
+    const urlID = req.url.split('/')[2];
+
+    fs.readFile(studentsFile, 'utf-8', (error, fileData) => {
+        if (error) {
+            console.log(error);
+            return;
+        }
+
+        const students = JSON.parse(fileData);
+        const studentInd = students.findIndex(student => student.id === urlID);
+        if (studentInd !== -1) {
+            const data = [];
+            
+            req.on('data', (chunk) => {
+                data.push(chunk);
+            })
+            
+            req.on('end', () => {
+                const bufferBody = Buffer.concat(data).toString();
+                const bodyOfRequest = JSON.parse(bufferBody)
+
+                students[studentInd].name = bodyOfRequest.name || students[studentInd].name;
+                // students[studentInd].name = bodyOfRequest.name ? bodyOfRequest.name : students[studentInd].name;
+                students[studentInd].department = bodyOfRequest.department || students[studentInd].department;
+                // students[studentInd].department = bodyOfRequest.department ? bodyOfRequest.department: students[studentInd].department;
+                students[studentInd].school = bodyOfRequest.school ||  students[studentInd].school;
+                // students[studentInd].school = bodyOfRequest.school ? bodyOfRequest.school : students[studentInd].school;
+
+                fs.writeFile(studentsFile, JSON.stringify(students, null, 4), 'utf-8', (error) => {
+                    if (error) {
+                        console.log(error);
+                        return;
+                    }
+
+                    res.writeHead(200).write(JSON.stringify(students[studentInd]));        
+                })
+            });
+
+        } else {
+            res.writeHead(404).write(`No Student with ${urlID} Exist!`);
+        }
+
         res.end();
     })
 }
